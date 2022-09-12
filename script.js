6f409e858bfdcc8504942da41dbd57cb96babdf5
umid <muzaffarov911gmail.com>
2022-09-12 23:01:40 +0500

    start

---
+++ script.js
@@ -0,0 +1,31 @@
+var name 
+var age = 26
+var bool = true
+var und;
+
+console.log( 'я ' + name + ' мне ' );
+
+var str = prompt (' Ввидите имя ')
+
+
+
+var str = prompt (' Ввидите возраст ')
+
+console.log(str);
+
+var str = prompt ('10+16=?')
+
+console.log('  ответ 26'); 
+
+var str = prompt ('10-1=?')
+
+console.log('  ответ 9'); 
+
+var str = prompt ('2*2=?')
+
+console.log('  ответ 4');
+
+var str = prompt ('16/2=?')
+
+console.log('  ответ 8');
+
